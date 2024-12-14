94c6cf67cb736fde1a1975366425f6f1c2beb63b
Your Name <your.email@example.com>
2024-12-13 19:45:34 -0600

    pod9

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
+    domains: [
+      'ipfs.io',
+      'gateway.ipfscdn.io',
+      'arweave.net',
+      'cloudflare-ipfs.com',
+      'ipfs.filebase.io',
+      'ipfs.infura.io',
+      'nft-cdn.alchemy.com',
+      'res.cloudinary.com',
+      'i.seadn.io',
+      'openseauserdata.com',
+      'metadata.ens.domains',
+      'storage.googleapis.com',
+      'sound.xyz',
+      'f4.bcbits.com',
+      'assets.sound.xyz',
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': './src',
-      '~': './src',
-    };
-    return config;
   },
 };
 
